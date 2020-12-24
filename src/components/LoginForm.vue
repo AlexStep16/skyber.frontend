3bf92bc6f7fac33217ef4fe4b98936a61a196730
alexander.work <alexander.work2020@gmail.com>
2020-12-25 01:04:38 +0300

    Start project

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <form action="api/login" class="form form_type-main">
+    <input type="text" name="email" id="email" class="input input_type-main pt4"><br>
+    <input type="text" name="email" id="email" class="input input_type-main">
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm'
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/common.blocks/input/_type/input_type-main.scss';
+@import '@/common.blocks/form/_type/form_type-main.scss';
+  
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="main">
+      <LoginForm />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    LoginForm
   }
 }
+
 </script>
+
+<style lang="scss">
+
+
+</style>
+
